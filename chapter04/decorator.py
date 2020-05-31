55c0d220dda4a89ae59a6aabf655a1d17283aec9
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-06-01 00:24:16 +0900

    Add 4.9

---
+++ chapter04/decorator.py
@@ -0,0 +1,26 @@
+def document_it(func):
+    def new_function(*args, **kwargs):
+        print('Running function:', func.__name__)
+        print('Positional arguments:', args)
+        print('Keyword arguments:', kwargs)
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
+
+def square_it(func):
+    def new_function(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * result
+    return new_function
+
+@document_it
+@square_it
+def add_ints(a, b):
+    return a + b
+
+cooler_add_ints = document_it(add_ints)
+cooler_add_ints(3, 5)
+
+add_ints(1, 2)
+
