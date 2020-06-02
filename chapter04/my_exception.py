185b4816b1d1bae82f352de41de49bd1f502beb6
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-06-03 00:13:59 +0900

    Add 4.12

---
+++ chapter04/my_exception.py
@@ -0,0 +1,13 @@
+#  class UppercaseException(Exception):
+    #  pass
+
+#  words = ['eeenie', 'meenie', 'miny', 'MO']
+#  for word in words:
+    #  if word.isupper():
+        #  raise UppercaseException(word)
+
+
+try:
+    raise Exception('panic')
+except Exception as exc:
+    print(exc)
