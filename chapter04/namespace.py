4faac9aa0a53d2c9d1ed9e3d6aacfb1ac421803a
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-06-02 23:57:58 +0900

    Add 4.10.1

--- chapter04/namespace.py
+++ chapter04/namespace.py
@@ -38,3 +38,12 @@ change_local()
 print('globals:', globals())
 
 print(animal)
+
+
+def amazing():
+    '''これはすばらしい関数だ。
+    もう一度見る?'''
+    print('関数の名前:', amazing.__name__)
+    print('docstring', amazing.__doc__)
+
+amazing()
