62d53898e43866581cb81915910aab902009a359
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-07-02 02:16:29 +0900

    Add 2.8.8

---
+++ chapter08/cfg_file.py
@@ -0,0 +1,7 @@
+import configparser
+cfg = configparser.ConfigParser()
+print(cfg.read('settings.cfg'))
+print(cfg)
+print(cfg['french'])
+print(cfg['french']['greeting'])
+print(cfg['files']['bin'])
